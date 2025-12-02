352bed43bde9aea9aca0b35dab81e60c4dd6f0fb
Ana Carolina Coronel <A00838713@tec.mx>
2025-12-02 10:37:36 -0600

    Update Interfaz_Servidor.js

--- Interfaz_Servidor.js
+++ Interfaz_Servidor.js
@@ -2,23 +2,37 @@ const express = require("express");
 const mqtt = require("mqtt");
 const http = require("http");
 const socketIo = require("socket.io");
+const { spawn } = require("child_process");
 
 const app = express();
+app.use( "/socket.io", express.static(__dirname + "/node_modules/socket.io/client-dist")
+);
 const server = http.createServer(app);
+
 const io = socketIo(server);
 
-//Express para almacenar .html
 app.use(express.static("pub"));
 app.use(express.json());
 
-//Conexión Mqtt
-const client = mqtt.connect("mqtt://192.168.246.15");
+const client = mqtt.connect("mqtt://10.147.13.15");
 
 client.on("connect", () => {
   console.log("Conectado al broker MQTT");
 });
+//Cámara
+app.get("/iniciar-cam", (req, res) => {
+  try {
+    const cam = spawn("python", ["cam.py"], {
+      cwd: __dirname,
+      detached: false,
+      stdio: ["ignore", "pipe", "pipe"],
+    });
+    cam.unref();
+    console.log("Cámara iniciada");
+    res.send("Cámara iniciada correctamente.");
+  } 
+});
 
-// Conexión Socket.io
 io.on("connection", (socket) => {
   console.log("Cliente conectado via Socket.io");
 
@@ -39,15 +53,14 @@ io.on("connection", (socket) => {
     console.log("Acción recibida", autom);
     client.publish("Motor/Direction", autom);
   });
-  //Estado ventilador
+
+
   socket.on("autoSensor", (status) => {
-    console.log("Acción recibida:", status);
     client.publish("Sensor/Estado", status);
   });
-  //Veloidad Ventilador
+
   socket.on("fanSpeed", (valor) => {
-    console.log("Acción recibida:", valor);
-    client.publish("Sensor/Estado", valor);
+    client.publish("Sensor/Velocidad", valor);
   });
 
   socket.on("disconnect", () => {
@@ -57,32 +70,34 @@ io.on("connection", (socket) => {
 
 client.on("message", (topic, message) => {
   const data = message.toString();
-  console.log(`MQTT recibido  ${topic}: ${data}`);
+  console.log(`MQTT recibido ${topic}: ${data}`);
 
-  //Senosres
   if (topic === "Sensor/Temperatura") {
     io.emit("sensor", { tipo: "temp", valor: data });
   } else if (topic === "Sensor/Humedad") {
     io.emit("sensor", { tipo: "hum", valor: data });
   } else if (topic === "Sensor/Ventilador") {
     io.emit("sensor", { tipo: "fan", valor: data });
-  } else if (topic === topicAuto) {
+  } else if (topic === "Sensor/Estado") {
     io.emit("autoStatus", { status: data });
-    console.log("Estado automático:", data);
-  } else if (topic === topicVelocidad) {
+  } else if (topic === "Sensor/Velocidad") {
     io.emit("fanSpeedUpdate", { speed: data });
-    console.log("Velocidad del ventilador:", data);
+  } else if (topic === "Camara/Live") {
+    io.emit("videoFrame", data);
+  } else if (topic === "Camara/Lectura") {
+    io.emit("qrDetectado", data);
   }
 });
 
-// Subscribirse a los tópicos
 client.subscribe("Sensor/Temperatura");
 client.subscribe("Sensor/Humedad");
 client.subscribe("Sensor/Ventilador");
 client.subscribe("Sensor/Estado");
 client.subscribe("Sensor/Velocidad");
+client.subscribe("Camara/Lectura");
+client.subscribe("Camara/Live");
+client.subscribe("Motor/Direction");
 
-//Iniciar el servidor
 server.listen(3000, () => {
   console.log("Servidor en http://localhost:3000");
 });
